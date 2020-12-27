2263bfff3e058ce8492a8159c9f7b420c2b930f6
WouterRademaker <egel@scoutnet.nl>
2020-12-27 17:00:27 +0100

    not template

--- includes/epsg.php
+++ includes/epsg.php
@@ -1,23 +1,17 @@
 <?php
 class EPSG {
 	static function EPSG_IO( $coord, $source = null, $target = null ) {
-    $array = explode(',', $coord);
-    $x = (empty($array[0])) ? null : $array[0];
-    $y = (empty($array[1])) ? null : $array[1];
+    $array = explode(',', preg_replace('/\s+/','',$coord));
+    $y = (empty($array[0])) ? null : $array[0];
+    $x = (empty($array[1])) ? null : $array[1];
     $z = (empty($array[2])) ? null : $array[2];
     $url = 'http://epsg.io/trans?s_srs='.$source.'&t_srs='.$target.'&x='.$x.'&y='.$y.'&z='.$z;
     $json = file_get_contents($url);
     return json_decode($json, true);
   }
 
-  static function WGS84ToEPSG( &$parser, $coord, $target, $template = null) {
+  static function WGS84ToEPSG( &$parser, $coord, $target) {
     $output=self::EPSG_IO( $coord, null , $target);
-		if (empty($template))
-    	{
-				return sprintf($output["x"].', '.$output["y"].', '.$output["z"]);
-			} else {
-				return sprintf('{{'.$template.'|'$output["x"].'|'.$output["y"].'|'.$output["z"].'}}');
-			}
-  }
-
+		return sprintf($output["x"].', '.$output["y"].', '.$output["z"]);
+	}
 }
