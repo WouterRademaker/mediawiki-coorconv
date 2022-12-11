41cdefeb3f03862fd61cf59ec6ae8accd4d0be46
Wouter Rademaker <egel@scoutnet.nl>
2022-12-11 17:18:06 +0100

    Update epsg.php
    Make it better able to deal with missing or null values

--- includes/epsg.php
+++ includes/epsg.php
@@ -2,20 +2,21 @@
 class EPSGIO {
 	static function EPSG_IO( $coord, $source = null, $target = null ) {
 		global $wgEPSG_URL;
-    $array = explode(',', preg_replace('/\s+/','',$coord));
-    $x = (empty($array[0])) ? null : $array[0];
-    $y = (empty($array[1])) ? null : $array[1];
-    $z = (empty($array[2])) ? null : $array[2];
-    $url = $EPSG_URL.'?s_srs='.$source.'&t_srs='.$target.'&x='.$x.'&y='.$y.'&z='.$z;
-    $json = file_get_contents($url);
-    return json_decode($json, true);
+    		$array = explode(',', preg_replace('/\s+/','',$coord));
+    		$x = (empty($array[0])) ? 0 : $array[0];
+   		$y = (empty($array[1])) ? 0 : $array[1];
+    		$z = (empty($array[2])) ? 0 : $array[2];
+    		$url = $wgEPSG_URL.'?x='.$x.'&y='.$y.'&z='.$z;
+    		if (!empty($source)) {$url .= '&s_srs='.$source;}
+    		if (!empty($target)) {$url .= '&t_srs='.$target;}
+    		$json = file_get_contents($url);
+    		return json_decode($json, true);
   }
-
-  static function WGS84ToEPSG( &$parser, $coord, $target) {
+  	static function WGS84ToEPSG( &$parser, $coord, $target) {
 		$array = explode(',', preg_replace('/\s+/','',$coord));
 		[$array[0], $array[1]] = [$array[1], $array[0]];
 		$coord = implode ( ',', $array );
-    $output=self::EPSG_IO( $coord, null , $target);
+    		$output=self::EPSG_IO( $coord, null , $target);
 		return sprintf($output["x"].', '.$output["y"].', '.$output["z"]);
 	}
 
